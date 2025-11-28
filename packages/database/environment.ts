99a6361183d7b2356572c5f8fa7d02de9906f5aa
Kevin Johnson <kevin@inventures.cl>
2025-11-28 11:48:46 -0300

    fix(database): simplify error message for DATABASE_URL validation

--- packages/database/environment.ts
+++ packages/database/environment.ts
@@ -2,7 +2,5 @@ export const NODE_ENV = process.env.NODE_ENV;
 
 export const DATABASE_URL = process.env.DATABASE_URL!;
 if (!DATABASE_URL) {
-  throw new Error(
-    "[Database] DATABASE_URL is not defined in the environment variables"
-  );
+  throw new Error("DATABASE_URL is not defined in the environment variables");
 }
