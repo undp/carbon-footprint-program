1161a2e5292502c3fe95878590ac6fb1c9498f16
Luis Aparicio <luis@inventures.cl>
2025-12-03 16:33:17 -0300

    feat: update Route component to use LandingScreen directly

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -2,5 +2,5 @@ import { createFileRoute } from "@tanstack/react-router";
 import { LandingScreen } from "@screens";
 
 export const Route = createFileRoute("/")({
-  component: () => <LandingScreen />,
+  component: LandingScreen,
 });
