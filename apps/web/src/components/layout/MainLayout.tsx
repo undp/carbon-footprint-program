61b6e48cd2bd9c7202cb16f2f7c0ebcd3d5d3afd
Luis Aparicio <luis@inventures.cl>
2025-12-03 16:10:17 -0300

    refactor: simplify MainLayout component by using PropsWithChildren

--- apps/web/src/components/layout/MainLayout.tsx
+++ apps/web/src/components/layout/MainLayout.tsx
@@ -1,15 +1,10 @@
+import { FC, PropsWithChildren } from "react";
 import { Box, Container } from "@mui/material";
 import { Sidebar } from "./Sidebar/Sidebar";
 
-interface MainLayoutProps {
-  children?: React.ReactNode;
-}
-
-export const MainLayout = ({ children }: MainLayoutProps) => {
-  return (
-    <Box className="flex">
-      <Sidebar />
-      <Container className="py-6">{children}</Container>
-    </Box>
-  );
-};
+export const MainLayout: FC<PropsWithChildren> = ({ children }) => (
+  <Box className="flex">
+    <Sidebar />
+    <Container className="py-6">{children}</Container>
+  </Box>
+);
