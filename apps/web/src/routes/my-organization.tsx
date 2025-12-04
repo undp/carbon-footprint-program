5741682ba76332eb28bfe392b11ea7990e3323aa
Luis Aparicio <luis@inventures.cl>
2025-12-04 16:27:51 -0300

    feat: add my-organization route with placeholder component

--- apps/web/src/routes/my-organization.tsx
+++ apps/web/src/routes/my-organization.tsx
@@ -2,11 +2,11 @@ import { createFileRoute } from "@tanstack/react-router";
 import { MainLayout } from "@/components/layout";
 import { Routes } from "@/interfaces/routes";
 
-export const Route = createFileRoute(Routes.MY_COMPANY)({
+export const Route = createFileRoute(Routes.MY_ORGANIZATION)({
   component: () => (
     <MainLayout>
       {/* TODO: Replace with real MyCompany screen component */}
-      <div>Hello &quot;/my-company&quot;!</div>
+      <div>Hello &quot;/my-organization&quot;!</div>
     </MainLayout>
   ),
 });
