fa979d5e42fe1819dacc98ab80f222b55c9e7db4
Kevin Johnson <kevin@inventures.cl>
2025-12-09 14:41:40 -0300

    fix: update DATABASE_URL handling in environment configuration
    - Changed DATABASE_URL assignment to use nullish coalescing, providing a default empty string if not defined.
    - Removed the error throw for undefined DATABASE_URL to improve flexibility in environment variable management.

--- packages/database/environment.ts
+++ packages/database/environment.ts
@@ -1,6 +1,3 @@
 export const NODE_ENV = process.env.NODE_ENV;
 
-export const DATABASE_URL = process.env.DATABASE_URL!;
-if (!DATABASE_URL) {
-  throw new Error("DATABASE_URL is not defined in the environment variables");
-}
+export const DATABASE_URL = process.env.DATABASE_URL ?? "";
