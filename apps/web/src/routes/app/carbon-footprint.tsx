f2f12b3407ce6f97c5b0bb41da0eea0e697f613e
Matías Rivas <matias.rivas@inventures.cl>
2025-12-11 08:24:32 -0300

    feat: implement BusinessProfilingScreen for carbon footprint calculation base inputs

--- apps/web/src/routes/app/carbon-footprint.tsx
+++ apps/web/src/routes/app/carbon-footprint.tsx
@@ -1,20 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { MainLayout } from "@/components/layout";
 import { Routes } from "@/interfaces/routes";
-import { useCountryOrganizationSizes } from "@/api/query";
+import { BusinessProfilingScreen } from "@/screens/FootprintCalculator/BusinessProfilingScreen";
 
 export const Route = createFileRoute(Routes.CARBON_FOOTPRINT)({
-  component: () => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const { data: countryOrganizationSizes } = useCountryOrganizationSizes();
-    return (
-      <MainLayout>
-        {/* TODO: Replace with real Organization Footprint screen component */}
-        <div>
-          Hello &quot;/carbon-footprint&quot;!{" "}
-          {countryOrganizationSizes?.map((size) => size.name).join(", ")}
-        </div>
-      </MainLayout>
-    );
-  },
+  component: () => <BusinessProfilingScreen />,
 });
