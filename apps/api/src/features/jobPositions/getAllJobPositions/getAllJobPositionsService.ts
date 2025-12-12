ad9e386fb2ef5b9b7ae7772dbb1f8762d84a5351
Kevin Johnson <kevin@inventures.cl>
2025-12-12 01:07:01 -0300

    fix(api): sort job positions by name in getAllJobPositionsService
    - Updated the `getAllJobPositionsService` to order job positions by name in ascending order when retrieving data from the database, improving the organization and readability of job position listings.

--- apps/api/src/features/jobPositions/getAllJobPositions/getAllJobPositionsService.ts
+++ apps/api/src/features/jobPositions/getAllJobPositions/getAllJobPositionsService.ts
@@ -4,7 +4,11 @@ import type { GetAllJobPositionsResponse } from "./getAllJobPositionsSchema.js";
 export const getAllJobPositionsService = async (
   prismaClient: PrismaClient
 ): Promise<GetAllJobPositionsResponse> => {
-  const data = await prismaClient.country_job_position.findMany();
+  const data = await prismaClient.country_job_position.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
   return data.map((item) => ({
     id: item.id.toString(),
     name: item.name,
