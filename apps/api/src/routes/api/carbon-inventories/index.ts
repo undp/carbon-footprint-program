e4fad5eb63bac846d237d41d4fe1caeab3fd5655
Kevin Johnson <kevin@inventures.cl>
2025-12-18 09:14:13 -0300

    [API] Feat: Implement carbon inventories routes
    - Added a new index file for carbon inventories routes, integrating handlers for retrieving all carbon inventories, fetching a carbon inventory by ID, and creating a new carbon inventory.
    - This enhancement improves the modularity and organization of the API, facilitating better management of carbon inventory-related functionalities.

---
+++ apps/api/src/routes/api/carbon-inventories/index.ts
@@ -0,0 +1,10 @@
+import type { FastifyZodInstance } from "@/types/fastify.js";
+import { getAllCarbonInventoriesRoute } from "@/features/carbonInventories/getAllCarbonInventories/getAllCarbonInventoriesRoute.js";
+import { getCarbonInventoryByIdRoute } from "@/features/carbonInventories/getCarbonInventoryById/getCarbonInventoryByIdRoute.js";
+import { createCarbonInventoryRoute } from "@/features/carbonInventories/createCarbonInventory/createCarbonInventoryRoute.js";
+
+export default function carbonInventoriesRoutes(fastify: FastifyZodInstance) {
+  getAllCarbonInventoriesRoute(fastify);
+  getCarbonInventoryByIdRoute(fastify);
+  createCarbonInventoryRoute(fastify);
+}
