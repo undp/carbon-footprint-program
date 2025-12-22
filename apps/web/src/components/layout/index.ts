35856d1598edfc22cfffcdcc96618f312128c038
Luis Aparicio <luis@inventures.cl>
2025-12-22 18:03:15 -0300

    Feat: Export BaseHeader component from layout index

--- apps/web/src/components/layout/index.ts
+++ apps/web/src/components/layout/index.ts
@@ -1,3 +1,4 @@
 export { MainLayout } from "./MainLayout";
 export { Sidebar } from "./Sidebar";
 export { AuthenticationLayout } from "./AuthenticationLayout";
+export { BaseHeader } from "./BaseHeader";
