565b50da8746dec832dbbb5d98061ad1dba1db82
Kevin Johnson <kevin@inventures.cl>
2025-12-23 16:42:20 -0300

    Feat: enhance validation error response schema in errors.ts
    - Added new fields to the ValidationErrorResponseSchema, including 'error' for error type and 'errors' for optional validation error details.
    - This update improves the structure and clarity of validation error responses, enhancing error handling across the API.

--- apps/api/src/commonSchemas/errors.ts
+++ apps/api/src/commonSchemas/errors.ts
@@ -3,6 +3,8 @@ import { z } from "zod";
 export const ValidationErrorResponseSchema = z
   .object({
     message: z.string().describe("Validation error message"),
+    error: z.string().describe("Error type"),
+    errors: z.array(z.any()).describe("Validation errors details").optional(),
   })
   .describe("Validation error response");
 
