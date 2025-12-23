fd425b297be95f1fc1cec176a175a5563c23bd4c
Kevin Johnson <kevin@inventures.cl>
2025-12-23 16:08:45 -0300

    Refactor: remove default values in createCarbonInventoryService
    - Eliminated default values for fields such as year, status, and organization-related IDs in the createCarbonInventoryService, aligning with recent schema updates and improving clarity in data handling.
    - Added TODOs for incorporating user IDs in future implementations, enhancing future functionality.

--- apps/api/src/features/carbonInventories/createCarbonInventory/createCarbonInventoryService.ts
+++ apps/api/src/features/carbonInventories/createCarbonInventory/createCarbonInventoryService.ts
@@ -11,15 +11,7 @@ export const createCarbonInventoryService = async (
 ): Promise<CreateCarbonInventoryResponse> => {
   const item = await prismaClient.carbon_inventory.create({
     data: {
-      year: null,
-      status: "DRAFT", // Default status
       usage_mode: data.usageMode,
-      is_editable: true, // Default to editable
-      organization_id: null,
-      organization_branch_id: null,
-      organization_data: Prisma.JsonNull,
-      methodology_version_id: null,
-      preselected_nodes_id: null,
       created_by_id: null, // TODO: Add created by id from logged in user
       updated_by_id: null, // TODO: Add updated by id from logged in user
     },
