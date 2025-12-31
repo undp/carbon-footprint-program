3a7caf8ed97b9a69f29cac6d7c9329026917574c
Matias Rivas <matias.rivas@inventures.cl>
2025-12-31 09:10:59 -0300

    feat: add multiple CategoryCard components for emission categories in SubCategoryPreselectionScreen

--- apps/web/src/screens/CarbonInventory/SubCategoryPreselectionScreen.tsx
+++ apps/web/src/screens/CarbonInventory/SubCategoryPreselectionScreen.tsx
@@ -4,6 +4,7 @@ import { useNavigate, useParams } from "@tanstack/react-router";
 import { CarbonInventoryLayout } from "./layout";
 import { Routes } from "@/interfaces";
 import { StepHeader } from "./components/StepHeader";
+import { CategoryCard } from "./components";
 
 export const SubCategoryPreselectionScreen: FC = () => {
   const navigate = useNavigate();
@@ -40,9 +41,30 @@ export const SubCategoryPreselectionScreen: FC = () => {
           <Box className="flex flex-row gap-6">
             <Box className="flex flex-1 flex-col gap-8">
               {/* Agregar campos del formulario aquí */}
+              <CategoryCard
+                position={1}
+                title="Alcance 1"
+                subtitle="Categoria 1 / Alcance 1"
+                description="Generadas dentro de tu empresa"
+              />
             </Box>
             <Box className="flex flex-1 flex-col gap-8">
               {/* Agregar campos del formulario aquí */}
+              <CategoryCard
+                position={2}
+                title="Alcance 2"
+                subtitle="Categoria 2 / Alcance 2"
+                description="Generadas por la electricidad consumida"
+              />
+            </Box>
+            <Box className="flex flex-1 flex-col gap-8">
+              {/* Agregar campos del formulario aquí */}
+              <CategoryCard
+                position={3}
+                title="Alcance 3"
+                subtitle="Categorias 3,4,5 y 6 / Alcance 3"
+                description="Generadas fuera de tu empresa"
+              />
             </Box>
           </Box>
         </Box>
