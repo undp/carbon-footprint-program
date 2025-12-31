f2d3c5b10a7bf7852f80e9b9084e8ac83aa45d37
Matias Rivas <matias.rivas@inventures.cl>
2025-12-31 08:50:39 -0300

    feat: add new emission category icons

--- apps/web/src/icons/index.ts
+++ apps/web/src/icons/index.ts
@@ -2,3 +2,6 @@ export { LatamFootprintIcon } from "./LatamFootprintIcon";
 export { HuellaLatamLogo } from "./HuellaLatamLogo";
 export { CalculatorIcon } from "./CalculatorIcon";
 export { ApplicationFormIcon } from "./ApplicationFormIcon";
+export { DirectEmissionCategoryIcon } from "./DirectEmissionCategoryIcon";
+export { IndirectEmissionCategoryIcon } from "./IndirectEmissionCategoryIcon";
+export { OthersCategoryIcon } from "./OthersCategoryIcon";
