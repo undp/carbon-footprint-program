b0e48f4316d82f776c10db976ede85344a071364
Matias Rivas <matias.rivas@inventures.cl>
2026-01-09 11:07:12 -0300

    Feat: Add Emission Summary screen and update routing

--- apps/web/src/routes/app/carbon-inventory/$inventoryId/emission-summary.tsx
+++ apps/web/src/routes/app/carbon-inventory/$inventoryId/emission-summary.tsx
@@ -1,8 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { EmissionSummaryScreen } from "@/screens/CarbonInventory/EmissionSummaryScreen";
+import { Routes } from "@/interfaces";
 
-export const Route = createFileRoute(
-  "/app/carbon-inventory/$inventoryId/emission-summary"
-)({
+export const Route = createFileRoute(Routes.CARBON_INVENTORY_EMISSION_SUMMARY)({
   component: EmissionSummaryScreen,
 });
