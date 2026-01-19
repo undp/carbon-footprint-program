2643e45759b15661b3acfb7925f64ab6e212db5c
Kevin Johnson <kevin@inventures.cl>
2026-01-19 10:05:27 -0300

    Refactor: Update seedUsers script to include email in UserData type and fix email mapping in user query

--- packages/database/src/prisma/seeds/scripts/seedUsers.ts
+++ packages/database/src/prisma/seeds/scripts/seedUsers.ts
@@ -13,8 +13,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 type UserData = (Required<
-  Pick<Prisma.UserCreateInput, "email" | "firstName" | "lastName">
+  Pick<Prisma.UserCreateInput, "firstName" | "lastName">
 > & {
+  email: string;
   countryJobPositionName: Prisma.CountryJobPositionCreateInput["name"];
   countryIsoCode: Prisma.CountryCreateInput["isoCode"];
 })[];
@@ -90,7 +91,7 @@ export async function seedUsers(prisma: PrismaClient, dataset: SeedsDataset) {
   const users = await prisma.user.findMany({
     where: {
       email: {
-        in: usersData.map((u) => u.email!),
+        in: usersData.map((u) => u.email),
       },
     },
   });
