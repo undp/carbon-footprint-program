0505db9c24e745eced1fba55f1088a4ea47f7937
Luis Aparicio <luis@inventures.cl>
2026-01-20 11:00:33 -0300

    Feat: Add user resolve plugin to handle user creation and attachment to request

---
+++ apps/api/src/plugins/app/userResolvePlugin.ts
@@ -0,0 +1,62 @@
+import fp from "fastify-plugin";
+import { FastifyPluginCallback, FastifyRequest } from "fastify";
+import { mapUserToResponse } from "../../features/users/mappers.js";
+
+const userResolvePlugin: FastifyPluginCallback = (fastify) => {
+  // Find or create user on the DB with the data of the authenticated user with oid and email and attach to request
+
+  fastify.addHook("preHandler", async function (request: FastifyRequest) {
+    const log = request.log.child({ module: "user-resolve-plugin" });
+
+    if (!request.authUser) {
+      log.debug(
+        "No authenticated user on request; skipping user request population"
+      );
+      return;
+    }
+    const prisma = request.server.prisma;
+    const authUser = request.authUser;
+
+    log.info(
+      {
+        idpUserId: authUser.idpUserId,
+      },
+      "Processing authenticated user"
+    );
+
+    // Find existing user by idpUserId
+    let user = await prisma.user.findUnique({
+      where: { idpUserId: authUser.idpUserId },
+    });
+
+    // If not found, create new user
+    if (!user) {
+      log.info(
+        { idpUserId: authUser.idpUserId },
+        "User not found; creating new user"
+      );
+      user = await prisma.user.create({
+        data: {
+          idpUserId: authUser.idpUserId,
+          email: authUser.email,
+        },
+      });
+
+      log.info({ userId: user.id }, "New user created");
+    } else {
+      log.info({ userId: user.id }, "Existing user found");
+    }
+
+    // Attach the user to the request
+    request.currentUser = mapUserToResponse(user);
+    log.debug({ userId: user.id }, "User attached to request");
+  });
+
+  fastify.log.info("User resolve plugin registered");
+};
+
+export default fp(userResolvePlugin, {
+  name: "user-resolve-plugin",
+  // Depend on JWT plugin for JWKS provider
+  dependencies: ["authentication-plugin"],
+});
