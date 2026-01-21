4f22f5b6fc84a8ff210c5941a18664dc97da769a
Luis Aparicio <luis@inventures.cl>
2026-01-21 11:34:20 -0300

    feat: enhance SignInScreen with loading state and sign-in functionality

--- apps/web/src/screens/Auth/Sign-In/SignInScreen.tsx
+++ apps/web/src/screens/Auth/Sign-In/SignInScreen.tsx
@@ -1,16 +1,32 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
-import { FC } from "react";
-import { Link } from "@tanstack/react-router";
-import { Route as RegisterRoute } from "@/routes/auth/sign-up";
+import { Box, Button, CircularProgress, Typography } from "@mui/material";
 import { AuthenticationLayout } from "@/components/layout";
+import { useAuth } from "@/contexts/AuthContext";
 
-export const SignInScreen: FC = () => {
+export const SignInScreen = () => {
+  const { signInRedirect, isLoading } = useAuth();
+
+  if (isLoading) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center" p={4}>
+        <CircularProgress />
+      </Box>
+    );
+  }
+
   return (
     <AuthenticationLayout>
-      <Box className="flex flex-col pt-20 px-10 gap-6 w-full justify-center items-center">
+      <Box className="flex w-full flex-col items-center justify-center gap-6 px-10 pt-20">
         <Typography color="common.deepForest" variant="h4" fontWeight="medium">
           Inicia sesión
         </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={signInRedirect}
+          size="large"
+        >
+          Iniciar Sesión
+        </Button>
 
         <Typography
           sx={{ maxWidth: 364 }}
@@ -20,7 +36,7 @@ export const SignInScreen: FC = () => {
         >
           Ingresa tu correo electrónico. Te enviaremos un código de verificación
         </Typography>
-
+        {/* 
         <TextField
           sx={{ width: 324 }}
           label="Correo electrónico"
@@ -36,7 +52,7 @@ export const SignInScreen: FC = () => {
           <Typography component={Link} color="info" to={RegisterRoute.to}>
             Crear cuenta
           </Typography>
-        </Typography>
+        </Typography> */}
       </Box>
     </AuthenticationLayout>
   );
