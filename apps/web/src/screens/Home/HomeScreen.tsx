e51a98586da02a8d535a00c8d4856808bb519c86
Luis Aparicio <luis@inventures.cl>
2026-01-27 14:12:39 -0300

    feat: implement HomeScreen component and update routing

---
+++ apps/web/src/screens/Home/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import { MainLayout } from "@/components";
+
+export const HomeScreen: FC = () => {
+  return (
+    <MainLayout>
+      {/* TODO: Replace with real Home screen component */}
+      <div>Hello &quot;/home&quot;!</div>
+    </MainLayout>
+  );
+};
--- apps/web/src/screens/index.ts
+++ apps/web/src/screens/index.ts
@@ -1,2 +1,3 @@
 export { LandingScreen } from "./Landing";
 export { SignInScreen, SignUpScreen } from "./Auth";
+export { HomeScreen } from "./Home";
