c3535858a5c6e0eba0f170f70a288d511a0c146e
Luis Aparicio <luis@inventures.cl>
2026-01-27 14:12:51 -0300

    feat: update Header component for user authentication and navigation

--- apps/web/src/screens/Landing/components/Header.tsx
+++ apps/web/src/screens/Landing/components/Header.tsx
@@ -7,6 +7,8 @@ import {
   LandingHeaderRoutesTranslations,
 } from "@/interfaces";
 import { Route as SignInRoute } from "@/routes/auth/sign-in";
+import { Route as HomeRoute } from "@/routes/app/home";
+import { useAuth } from "../../../contexts";
 
 const pages = Object.values(LandingHeaderRoutes).map((route) => ({
   text: LandingHeaderRoutesTranslations[route],
@@ -16,11 +18,15 @@ const pages = Object.values(LandingHeaderRoutes).map((route) => ({
 export const Header: FC = () => {
   const theme = useTheme();
   const navigate = useNavigate();
+  const { account } = useAuth();
 
-  const onClickSignIn = useCallback(
-    () => void navigate({ to: SignInRoute.to }),
-    [navigate]
-  );
+  const onClick = useCallback(() => {
+    if (account) {
+      void navigate({ to: HomeRoute.to });
+    } else {
+      void navigate({ to: SignInRoute.to });
+    }
+  }, [navigate, account]);
 
   return (
     <AppBar color="transparent" elevation={0} position="static">
@@ -44,13 +50,23 @@ export const Header: FC = () => {
             </Link>
           ))}
         </Box>
-        <Button
-          sx={{ backgroundColor: theme.palette.common.deepForest }}
-          variant="contained"
-          onClick={onClickSignIn}
-        >
-          INICIAR SESIÓN
-        </Button>
+        {account ? (
+          <Link
+            className="text-base font-medium text-white no-underline"
+            key={"home-dashboard"}
+            to={HomeRoute.to}
+          >
+            Ir al Home
+          </Link>
+        ) : (
+          <Button
+            sx={{ backgroundColor: theme.palette.common.deepForest }}
+            variant="contained"
+            onClick={onClick}
+          >
+            INICIAR SESIÓN
+          </Button>
+        )}
       </Toolbar>
     </AppBar>
   );
