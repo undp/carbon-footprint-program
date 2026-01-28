03084c84d01d592ce3dcc45e0907a8b5677a77e1
Luis Aparicio <luis@inventures.cl>
2026-01-28 15:38:27 -0300

    feat: add termsAccepted and termsAcceptedAt fields to user creation process

--- packages/types/src/users/base.ts
+++ packages/types/src/users/base.ts
@@ -28,7 +28,6 @@ export const UserSchema = z.object({
   ),
   termsAccepted: z
     .boolean()
-    .nullable()
     .describe("Whether the user has accepted the terms and conditions"),
   termsAcceptedAt: z.iso
     .datetime()
