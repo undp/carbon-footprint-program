bd358bf2037092e486a478021e2c4f8cc3fc3a24
Marcelo Saldías <marcelo.saldias@inventures.cl>
2026-01-29 13:05:08 -0300

    feat: updated styles for ToggleCell

--- apps/web/src/screens/Maintainer/components/ToggleCell.tsx
+++ apps/web/src/screens/Maintainer/components/ToggleCell.tsx
@@ -1,16 +1,61 @@
 import { FC } from "react";
-import { Switch } from "@mui/material";
+import { Switch, useTheme } from "@mui/material";
 
 interface Props {
   value: boolean;
   onChange: (checked: boolean) => void;
 }
 
-export const ToggleCell: FC<Props> = ({ value, onChange }) => (
-  <Switch
-    checked={value}
-    onChange={(_e, checked) => onChange(checked)}
-    color="primary"
-    size="small"
-  />
-);
+export const ToggleCell: FC<Props> = ({ value, onChange }) => {
+  const theme = useTheme();
+  return (
+    <Switch
+      checked={value}
+      onChange={(_e, checked) => onChange(checked)}
+      sx={{
+        width: 42,
+        height: 26,
+        padding: 0,
+        "& .MuiSwitch-switchBase": {
+          padding: 0,
+          paddingLeft: "4px",
+          paddingTop: "4px",
+          transitionDuration: "300ms",
+          "&.Mui-checked": {
+            transform: "translateX(15px)",
+            color: "#fff",
+            "& + .MuiSwitch-track": {
+              opacity: 1,
+              border: 0,
+            },
+            "&.Mui-disabled + .MuiSwitch-track": {
+              opacity: 0.5,
+            },
+          },
+          "&.Mui-focusVisible .MuiSwitch-thumb": {
+            border: "6px solid #fff",
+          },
+          "&.Mui-disabled .MuiSwitch-thumb": {
+            color: theme.palette.grey[100],
+          },
+          "&.Mui-disabled + .MuiSwitch-track": {
+            opacity: 0.7,
+          },
+        },
+        "& .MuiSwitch-thumb": {
+          boxSizing: "border-box",
+          width: 18,
+          height: 18,
+        },
+        "& .MuiSwitch-track": {
+          borderRadius: 26 / 2,
+          backgroundColor: "#E9E9EA",
+          opacity: 1,
+          transition: theme.transitions.create(["background-color"], {
+            duration: 500,
+          }),
+        },
+      }}
+    />
+  );
+};
