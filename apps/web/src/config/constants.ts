02cdb6b8bb2778bf77cc35382df51ce367ddc16a
Luis Aparicio <luis@inventures.cl>
2026-02-02 16:04:05 -0300

    Feat: Add REFETCH_INTERVAL_MS constant for consistent fetch timing

--- apps/web/src/config/constants.ts
+++ apps/web/src/config/constants.ts
@@ -1,5 +1,6 @@
 export const STALE_TIME_MS = 1000 * 60 * 5; // 5 minutes
 export const CALCULATOR_YEARS_RANGE_FROM_CURRENT = 5;
+export const REFETCH_INTERVAL_MS = 1000 * 60 * 5; // 5 minutes
 
 // TODO: fetch from API
 export const CUSTOM_FACTOR_SOURCES = ["Factor Propio", "Otro"];
