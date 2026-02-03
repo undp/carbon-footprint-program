6ef2881f753884b915f60b1c272602404e3a04fd
Luis Aparicio <luis@inventures.cl>
2026-02-03 17:31:43 -0300

    Feat: Update MainLayout component styles for improved layout

--- apps/web/src/components/layout/MainLayout.tsx
+++ apps/web/src/components/layout/MainLayout.tsx
@@ -3,8 +3,8 @@ import { Box, Container } from "@mui/material";
 import { Sidebar } from "./Sidebar/Sidebar";
 
 export const MainLayout: FC<PropsWithChildren> = ({ children }) => (
-  <Box className="flex">
+  <Box className="pl-[268px]">
     <Sidebar />
-    <Container className="py-6">{children}</Container>
+    <Container className="px-6 py-6">{children}</Container>
   </Box>
 );
