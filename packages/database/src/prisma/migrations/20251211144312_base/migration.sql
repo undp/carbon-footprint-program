b2223cf9a4bbfb6bd24f28a85629301ba983af05
Luis Aparicio <luis@inventures.cl>
2026-02-03 10:47:14 -0300

    Fix: Set default value for terms_accepted column in user table

--- packages/database/src/prisma/migrations/20251211144312_base/migration.sql
+++ packages/database/src/prisma/migrations/20251211144312_base/migration.sql
@@ -130,7 +130,7 @@ CREATE TABLE "user" (
     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "created_by_id" BIGINT,
     "updated_by_id" BIGINT,
-    "terms_accepted" BOOLEAN,
+    "terms_accepted" BOOLEAN NOT NULL DEFAULT false,
     "terms_accepted_at" TIMESTAMP(3),
 
     CONSTRAINT "user_pkey" PRIMARY KEY ("id")
