a89e8de466ea42c9547b111d8e8f08869f84b684
Luis Aparicio <luis@inventures.cl>
2026-02-04 15:32:27 -0300

    Refactor: Simplify component rendering for CarbonInventoriesScreen

--- apps/web/src/routes/app/carbon-inventories.tsx
+++ apps/web/src/routes/app/carbon-inventories.tsx
@@ -2,5 +2,5 @@ import { createFileRoute } from "@tanstack/react-router";
 import { Routes } from "@/interfaces";
 import { CarbonInventoriesScreen } from "@/screens/CarbonInventories";
 export const Route = createFileRoute(Routes.CARBON_INVENTORIES)({
-  component: () => <CarbonInventoriesScreen />,
+  component: CarbonInventoriesScreen,
 });
