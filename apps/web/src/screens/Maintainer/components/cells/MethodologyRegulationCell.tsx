0c08947304760eb3f7aad3f5f1016944f8fd675e
Marcelo Saldías <marcelo.saldias@inventures.cl>
2026-02-05 18:41:40 -0300

    feat: save changes locally, only sync with form onBlur

--- apps/web/src/screens/Maintainer/components/cells/MethodologyRegulationCell.tsx
+++ apps/web/src/screens/Maintainer/components/cells/MethodologyRegulationCell.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 import { useWatch, useFormState, useFormContext } from "react-hook-form";
 import { Autocomplete, TextField, Typography, Tooltip } from "@mui/material";
 import { NORMATIVA_OPTIONS } from "../../constants";
@@ -18,7 +18,7 @@ export const MethodologyRegulationCell: FC<MethodologyRegulationCellProps> = ({
   onClick,
 }) => {
   const { control } = useFormContext<MethodologiesFormValues>();
-  const value = useWatch<MethodologiesFormValues>({
+  const formValue = useWatch<MethodologiesFormValues>({
     name: `methodologies.${rowIndex}.regulation`,
   }) as string;
   const { errors } = useFormState({
@@ -27,25 +27,38 @@ export const MethodologyRegulationCell: FC<MethodologyRegulationCellProps> = ({
   });
   const fieldError = errors.methodologies?.[rowIndex]?.regulation;
 
+  const [localValue, setLocalValue] = useState(formValue);
+
+  // Sync local state when form value changes externally
+  useEffect(() => {
+    setLocalValue(formValue);
+  }, [formValue]);
+
   if (!isEditing) {
     return (
-      <Typography
-        onClick={onClick}
-        sx={{
-          px: 1,
-          py: 0.5,
-          borderRadius: 1,
-          cursor: onClick ? "pointer" : "default",
-          transition: "background-color 0.15s ease",
-          "&:hover": onClick
-            ? {
-                backgroundColor: "grey.100",
-              }
-            : {},
-        }}
-      >
-        {value}
-      </Typography>
+      <Tooltip title={formValue} arrow placement="top" enterDelay={500}>
+        <Typography
+          onClick={onClick}
+          sx={{
+            px: 1,
+            py: 0.5,
+            borderRadius: 1,
+            cursor: onClick ? "pointer" : "default",
+            transition: "background-color 0.15s ease",
+            "&:hover": onClick
+              ? {
+                  backgroundColor: "grey.100",
+                }
+              : {},
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            whiteSpace: "nowrap",
+            width: "100%",
+          }}
+        >
+          {formValue}
+        </Typography>
+      </Tooltip>
     );
   }
 
@@ -56,9 +69,10 @@ export const MethodologyRegulationCell: FC<MethodologyRegulationCellProps> = ({
         fullWidth
         size="small"
         options={NORMATIVA_OPTIONS.map((option) => option.value)}
-        value={value}
-        onChange={(_, newValue) => onChange(newValue ?? "")}
-        onInputChange={(_, newInputValue) => onChange(newInputValue)}
+        value={localValue}
+        onChange={(_, newValue) => setLocalValue(newValue ?? "")}
+        onInputChange={(_, newInputValue) => setLocalValue(newInputValue)}
+        onBlur={() => onChange(localValue)}
         sx={{
           "& .MuiAutocomplete-inputRoot": {
             py: 0,
@@ -68,6 +82,7 @@ export const MethodologyRegulationCell: FC<MethodologyRegulationCellProps> = ({
           <TextField
             {...params}
             error={!!fieldError}
+            onKeyDown={(e) => e.stopPropagation()}
             sx={{
               "& .MuiOutlinedInput-root": {
                 backgroundColor: "white",
