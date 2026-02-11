80224deaf2e7e053055dd98f1fb7fb203c9caf4d
Matias Rivas <matias.rivas@inventures.cl>
2026-02-11 14:38:01 -0300

    refactor: export CountrySubsectorSchema and SubmissionStatus enum

--- packages/types/src/countrySectors/getAllCountrySectors/schemas.ts
+++ packages/types/src/countrySectors/getAllCountrySectors/schemas.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { IdSchema } from "../../zod.js";
 
-const CountrySubsectorSchema = z.object({
+export const CountrySubsectorSchema = z.object({
   id: IdSchema.describe("The ID of the subsector"),
   name: z.string().min(1).describe("The name of the subsector"),
 });
