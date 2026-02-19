9bfba5fb17116a26ecb045a86326484531baa8e0
Marcelo Saldías <marcelo.saldias@inventures.cl>
2026-02-19 16:25:28 -0300

    [WEB] feat: enable react query devtool extension

--- apps/web/src/api/query/client.ts
+++ apps/web/src/api/query/client.ts
@@ -1,4 +1,5 @@
 import { QueryClient } from "@tanstack/react-query";
+import { IS_DEVELOPMENT } from "../../config/environment";
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -11,3 +12,15 @@ export const queryClient = new QueryClient({
     },
   },
 });
+
+// This code is only for TypeScript
+declare global {
+  interface Window {
+    __TANSTACK_QUERY_CLIENT__: QueryClient;
+  }
+}
+
+// This code is for all users
+if (IS_DEVELOPMENT) {
+  window.__TANSTACK_QUERY_CLIENT__ = queryClient;
+}
