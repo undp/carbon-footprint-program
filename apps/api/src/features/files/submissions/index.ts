aac739f42aad9cf9fc638117e64a68e526d83939
Luis Aparicio <luis@inventures.cl>
2026-02-24 18:01:42 -0300

    Feat: Rename submissions plugin to submissionsFilesRoutes for clarity

--- apps/api/src/features/files/submissions/index.ts
+++ apps/api/src/features/files/submissions/index.ts
@@ -3,7 +3,7 @@ import { submissionRequestUploadRoute } from "./requestSubmissionUpload/route.js
 import { submissionConfirmUploadRoute } from "./confirmSubmissionUpload/route.js";
 import { submissionGetFilesRoute } from "./getSubmissionFiles/route.js";
 
-export default function submissionsPlugin(fastify: FastifyZodInstance) {
+export default function submissionsFilesRoutes(fastify: FastifyZodInstance) {
   submissionRequestUploadRoute(fastify);
   submissionConfirmUploadRoute(fastify);
   submissionGetFilesRoute(fastify);
