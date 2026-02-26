4c295ece76fe361c6f58c6a0ab64ae82ee784c87
Luis Aparicio <luis@inventures.cl>
2026-02-26 11:03:46 -0300

    Feat: Change import statement to include types for DeleteFile schemas

--- packages/types/src/files/deleteFile/types.ts
+++ packages/types/src/files/deleteFile/types.ts
@@ -1,8 +1,5 @@
 import { z } from "zod";
-import type {
-  DeleteFileParamsSchema,
-  DeleteFileResponseSchema,
-} from "./schemas.js";
+import { DeleteFileParamsSchema, DeleteFileResponseSchema } from "./schemas.js";
 
 export type DeleteFileParams = z.infer<typeof DeleteFileParamsSchema>;
 export type DeleteFileResponse = z.infer<typeof DeleteFileResponseSchema>;
