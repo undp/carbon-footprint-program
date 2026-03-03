ac0f0a6ede048a70d6b40c4155f35e3b0ae541dc
Kevin Johnson <kevin@inventures.cl>
2026-03-03 11:03:35 -0300

    Refactor: simplify GetAllMeasurementUnitsResponseSchema by removing unnecessary field picks

--- packages/types/src/measurementUnits/getAllMeasurementUnits/schemas.ts
+++ packages/types/src/measurementUnits/getAllMeasurementUnits/schemas.ts
@@ -2,12 +2,5 @@ import { z } from "zod";
 import { MeasurementUnitBaseSchema } from "../../baseSchemas/index.js";
 
 export const GetAllMeasurementUnitsResponseSchema = z.array(
-  MeasurementUnitBaseSchema.pick({
-    id: true,
-    name: true,
-    magnitude: true,
-    abbreviation: true,
-    baseFactor: true,
-    isBase: true,
-  })
+  MeasurementUnitBaseSchema
 );
