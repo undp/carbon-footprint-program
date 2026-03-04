b5e3bbcf5914e107cd1ff5b2019a4202301ada18
Luis Aparicio <luis@inventures.cl>
2026-03-04 14:56:41 -0300

    Feat: add public option to badge routes for enhanced access control

--- apps/api/src/features/files/badges/index.ts
+++ apps/api/src/features/files/badges/index.ts
@@ -4,7 +4,7 @@ import { badgeConfirmUploadRoute } from "./confirmBadgeUpload/route.js";
 import { badgeGetFilesRoute } from "./getBadgeFiles/route.js";
 
 export default function badgeRoutes(fastify: FastifyZodInstance) {
-  badgeRequestUploadRoute(fastify);
-  badgeConfirmUploadRoute(fastify);
-  badgeGetFilesRoute(fastify);
+  badgeRequestUploadRoute(fastify, { public: true });
+  badgeConfirmUploadRoute(fastify, { public: true });
+  badgeGetFilesRoute(fastify, { public: true });
 }
