f9e8e69b9a0a9fda9ba46b763dba0494853889cd
Luis Aparicio <luis@inventures.cl>
2026-03-04 16:34:29 -0300

    Feat: add TODO comment to clarify future requirement for created_by_id in file table

--- packages/database/src/prisma/migrations/20260216171318_add_file_tables/migration.sql
+++ packages/database/src/prisma/migrations/20260216171318_add_file_tables/migration.sql
@@ -15,6 +15,7 @@ CREATE TABLE "file" (
     "status" "file_status" NOT NULL DEFAULT 'ACTIVE',
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "deleted_at" TIMESTAMP(3),
+    --TODO: This should be not null once we don't have to backfill badges with null createdById
     "created_by_id" BIGINT,
 
     CONSTRAINT "file_pkey" PRIMARY KEY ("id")
