c32921c30dd5864791ccf0343168cf7450bd71e1
Luis Aparicio <luis@inventures.cl>
2026-03-10 09:52:24 -0300

    Feat: Refactor HomeScreen to use HomeScreenContentSkeleton for loading state

--- apps/web/src/screens/Home/HomeScreen.tsx
+++ apps/web/src/screens/Home/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import { FC, useEffect, useMemo, useState } from "react";
-import { Box, Skeleton } from "@mui/material";
+import { Box } from "@mui/material";
 import { useCarbonInventories } from "../../api/query";
 import { Header, NoneCarbonInventoriesSection } from "./components";
 import { filter, map, orderBy, uniq } from "lodash-es";
@@ -9,6 +9,7 @@ import {
 } from "@repo/types";
 import { EmissionResultsContent } from "@/components";
 import { NoneVerifyCarbonInventories } from "./components/NoneVerifyCarbonInventories";
+import { HomeScreenContentSkeleton } from "./components/Skeletons/HomeContentSkeleton";
 
 export const HomeScreen: FC = () => {
   const [selectedYear, setSelectedYear] = useState<string>("");
@@ -108,25 +109,3 @@ export const HomeScreen: FC = () => {
     </Box>
   );
 };
-
-const HomeScreenContentSkeleton: FC = () => (
-  <Box className="flex min-h-0 flex-1 flex-row gap-4 rounded-lg bg-white p-6">
-    <Box className="flex min-h-0 flex-3 flex-col gap-4">
-      <Box className="flex min-h-0 flex-1 flex-row gap-4">
-        <Box className="flex min-h-0 flex-3 flex-col gap-3">
-          {[1, 2, 3, 4].map((i) => (
-            <Skeleton key={i} variant="rounded" height={56} />
-          ))}
-        </Box>
-        <Box className="flex min-h-0 flex-1">
-          <Skeleton variant="rounded" width="100%" height="100%" />
-        </Box>
-      </Box>
-      <Box className="flex min-h-0 flex-1 flex-row gap-4">
-        <Skeleton variant="rounded" sx={{ flex: 1 }} height="100%" />
-        <Skeleton variant="rounded" sx={{ flex: 1 }} height="100%" />
-      </Box>
-    </Box>
-    <Skeleton variant="rounded" sx={{ flex: 1 }} height="100%" />
-  </Box>
-);
