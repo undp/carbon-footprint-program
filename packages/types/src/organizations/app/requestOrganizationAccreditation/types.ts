8c14f76625b3ebd7a5c9a69b1d3d1dec65e344fc
Luis Aparicio <luis@inventures.cl>
2026-03-11 11:00:22 -0300

    feat: update schemas and types for organization accreditation to include file UUIDs

--- packages/types/src/organizations/app/requestOrganizationAccreditation/types.ts
+++ packages/types/src/organizations/app/requestOrganizationAccreditation/types.ts
@@ -1,11 +1,16 @@
 import { z } from "zod";
 import {
+  RequestOrganizationAccreditationBodySchema,
   RequestOrganizationAccreditationParamsSchema,
   RequestOrganizationAccreditationResponseSchema,
 } from "./schemas.js";
 
 export type RequestOrganizationAccreditationParams = z.infer<
   typeof RequestOrganizationAccreditationParamsSchema
+>;
+
+export type RequestOrganizationAccreditationBody = z.infer<
+  typeof RequestOrganizationAccreditationBodySchema
 >;
 
 export type RequestOrganizationAccreditationResponse = z.infer<
