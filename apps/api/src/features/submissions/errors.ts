1a3a9b59017abc3e8f015c3cffd9d56a698cd155
Luis Aparicio <luis@inventures.cl>
2026-04-01 14:55:10 -0300

    Refactor: update error message in SubmissionForbiddenError to specify organization access

--- apps/api/src/features/submissions/errors.ts
+++ apps/api/src/features/submissions/errors.ts
@@ -2,6 +2,6 @@ import createError from "@fastify/error";
 
 export const SubmissionForbiddenError = createError(
   "SUBMISSION_FORBIDDEN",
-  "You do not have access to submission %s",
+  "You do not have access to organization %s",
   403
 );
