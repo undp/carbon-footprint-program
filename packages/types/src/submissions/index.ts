66e85a3e5eca9623d7544c6193ac6bc226e0dc72
Luis Aparicio <luis@inventures.cl>
2026-04-07 10:40:45 -0400

    feat: remove submission history schemas and types from exports

--- packages/types/src/submissions/index.ts
+++ packages/types/src/submissions/index.ts
@@ -4,5 +4,3 @@ export * from "./getCarbonInventoryHistory/schemas.js";
 export * from "./getCarbonInventoryHistory/types.js";
 export * from "./getOrganizationHistory/schemas.js";
 export * from "./getOrganizationHistory/types.js";
-export * from "./getSubmissionHistory/schemas.js";
-export * from "./getSubmissionHistory/types.js";
