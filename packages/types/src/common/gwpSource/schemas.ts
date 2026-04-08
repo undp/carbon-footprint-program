fe83c4d0dcc4d622142a27d4550fc9bc8d5f7ca8
Matias Rivas <matias.rivas@inventures.cl>
2026-04-08 15:57:27 -0400

    Refactor: Add comments to schemas for clarity on enum deletion implications
    - Introduced comments in ConsideredGeiSchema, GwpSourceSchema, and IconNameSchema to clarify the potential impact of deleting enum entries on application functionality.
    - Emphasized the need to manage related database data before modifying enum entries, enhancing developer awareness and preventing errors.

--- packages/types/src/common/gwpSource/schemas.ts
+++ packages/types/src/common/gwpSource/schemas.ts
@@ -1,5 +1,8 @@
 import { z } from "zod";
 
+// Deleting any entries from this enum could potentially break the application.
+// This enum is used to type and parse the gwp source from the database.
+// if you need to delete an entry, you must first delete or modify the related data from the database.
 export const GwpSourceSchema = z.enum(["IPCC_AR4", "IPCC_AR5", "IPCC_AR6"]);
 
 export const GwpSourceEnum = GwpSourceSchema.enum;
