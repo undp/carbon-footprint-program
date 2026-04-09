d7fa22c300a265a3757f53e95ed2d078013996ac
Matias Rivas <matias.rivas@inventures.cl>
2026-04-09 10:32:19 -0400

    Update documentation comment for first-time submission in reduction project verification function
    - Changed the comment in the canRequestReductionProjectVerification function to clarify that it pertains to first-time submissions, enhancing code readability and understanding.

--- packages/utils/src/reductionProject.ts
+++ packages/utils/src/reductionProject.ts
@@ -18,7 +18,7 @@ export function isReductionProjectDeletable(
   return status === ReductionProjectDisplayStatusEnum.DRAFT;
 }
 
-/** First-time or resubmit after rejection. */
+/** First-time submission. */
 export function canRequestReductionProjectVerification(
   status: ReductionProjectDisplayStatus
 ): boolean {
