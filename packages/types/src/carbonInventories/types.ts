927cb2ca816ba6a350949ea8b4012e45245d6ee2
Luis Aparicio <luis@inventures.cl>
2026-04-20 11:32:18 -0400

    feat: change import statement for SubmissionType to a named import

--- packages/types/src/carbonInventories/types.ts
+++ packages/types/src/carbonInventories/types.ts
@@ -1,6 +1,6 @@
 import type { z } from "zod";
 import type { CarbonInventoryDisplayStatusSchema } from "./schemas.js";
-import type { SubmissionType } from "../enums.js";
+import { SubmissionType } from "../enums.js";
 
 export type CarbonInventoryDisplayStatus = z.infer<
   typeof CarbonInventoryDisplayStatusSchema
@@ -10,3 +10,10 @@ export type CarbonInventoryRecognitionsType = Exclude<
   SubmissionType,
   "ORGANIZATION_ACCREDITATION"
 >;
+
+export const RECOGNITION_SUBMISSION_TYPES: CarbonInventoryRecognitionsType[] = [
+  SubmissionType.CARBON_INVENTORY_CALCULATION,
+  SubmissionType.CARBON_INVENTORY_VERIFICATION,
+  SubmissionType.REDUCTION_PROJECT_VERIFICATION,
+  SubmissionType.NEUTRALIZATION_PLAN_VERIFICATION,
+];
