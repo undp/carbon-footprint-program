217385313bf42202e40462309f4467b5dcd2e225
Kevin Johnson <kevin@inventures.cl>
2026-05-04 15:09:41 -0400

    Refactor: remove prismaRetry helper

--- apps/api/src/utils/index.ts
+++ apps/api/src/utils/index.ts
@@ -2,6 +2,5 @@ export * from "./bigint.js";
 export * from "./decimal.js";
 export * from "./number.js";
 export * from "./string.js";
-export * from "./prismaRetry.js";
 
 export { CUSTOM_FACTOR_SOURCES } from "@repo/utils";
