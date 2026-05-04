41adc57a3a1efad406de26e3c47ad916fc4e69d9
Kevin Johnson <kevin@inventures.cl>
2026-05-04 14:17:37 -0400

    Refactor: removing the redundant Box wrapper

--- apps/web/src/screens/Maintainer/screens/Users/components/UsersScreenTable.tsx
+++ apps/web/src/screens/Maintainer/screens/Users/components/UsersScreenTable.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-import { Box } from "@mui/material";
 import { StylizedDataGrid } from "@components";
 import type { GetAllUsersResponse } from "@repo/types";
 import type { GridColDef } from "@mui/x-data-grid";
@@ -15,38 +14,36 @@ export const UsersScreenTable: FC<UsersScreenTableProps> = ({
   columns,
   isLoading,
 }) => (
-  <Box>
-    <StylizedDataGrid
-      sx={(theme) => ({
-        backgroundColor: "background.paper",
-        border: "none",
-        boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.08)",
-        "& .MuiDataGrid-main": {
-          padding: "16px !important",
-        },
-        "& .MuiDataGrid-columnHeader": {
-          backgroundColor: theme.palette.background.default,
-        },
-        "& .MuiDataGrid-cell": {
-          minHeight: "65px",
-          padding: "10px",
-        },
-      })}
-      loading={isLoading}
-      disableColumnMenu={false}
-      disableColumnFilter={false}
-      showToolbar
-      columns={columns}
-      rows={rows}
-      getRowHeight={() => "auto"}
-      getRowId={(row: GetAllUsersResponse[number]) => row.id}
-      disableColumnSorting={false}
-      hideFooter={false}
-      pagination
-      pageSizeOptions={[10, 25, 50, 100]}
-      initialState={{
-        pagination: { paginationModel: { pageSize: 10 } },
-      }}
-    />
-  </Box>
+  <StylizedDataGrid
+    sx={(theme) => ({
+      backgroundColor: "background.paper",
+      border: "none",
+      boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.08)",
+      "& .MuiDataGrid-main": {
+        padding: "16px !important",
+      },
+      "& .MuiDataGrid-columnHeader": {
+        backgroundColor: theme.palette.background.default,
+      },
+      "& .MuiDataGrid-cell": {
+        minHeight: "65px",
+        padding: "10px",
+      },
+    })}
+    loading={isLoading}
+    disableColumnMenu={false}
+    disableColumnFilter={false}
+    showToolbar
+    columns={columns}
+    rows={rows}
+    getRowHeight={() => "auto"}
+    getRowId={(row: GetAllUsersResponse[number]) => row.id}
+    disableColumnSorting={false}
+    hideFooter={false}
+    pagination
+    pageSizeOptions={[10, 25, 50, 100]}
+    initialState={{
+      pagination: { paginationModel: { pageSize: 10 } },
+    }}
+  />
 );
