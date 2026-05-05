3924c46394e438a1b55c75500fc14193cf850be8
Kevin Johnson <kevin@inventures.cl>
2026-05-05 16:18:40 -0400

    Refactor: remove formatting shated util

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,6 +1,5 @@
 export { kgToTon, tonToKg } from "./number.js";
 export { CUSTOM_FACTOR_SOURCES } from "./constants.js";
-export { formatEmissionFactor } from "./formatting.js";
 export {
   isCarbonInventoryEditable,
   isCarbonInventoryDeletable,
