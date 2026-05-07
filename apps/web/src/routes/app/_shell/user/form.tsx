19d7fe272e6c9fa5a1bb867b81fa5c25020714ec
Kevin Johnson <kevin@inventures.cl>
2026-05-07 15:07:53 -0400

    Refactor: prefer alias import over deep relative traversal.

--- apps/web/src/routes/app/_shell/user/form.tsx
+++ apps/web/src/routes/app/_shell/user/form.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 import { RouteIds } from "@/interfaces";
-import { UserFormScreen } from "../../../../screens/User";
+import { UserFormScreen } from "@/screens/User";
 
 export const Route = createFileRoute(RouteIds.USER_FORM)({
   component: () => <UserFormScreen />,
